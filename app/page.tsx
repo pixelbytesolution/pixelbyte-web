328c3db7602ef06ede681477a2254cd662584851
Anees <aneespengad4447@gmail.com>
2026-05-05 11:01:55 +0530

    feat(page): add about section to homepage - import and render about component - display information about the project or author

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/about/About";
 import Hero from "@/components/hero/Hero";
 import Navbar from "@/components/navbar/Navbar";
 import React from "react";
@@ -7,6 +8,7 @@ const page = () => {
     <main className="relative h-screen">
       <Navbar />
       <Hero />
+      <About />
     </main>
   );
 };
