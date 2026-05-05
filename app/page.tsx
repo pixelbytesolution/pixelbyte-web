37ed84a1df3032e293d44a9038862a01919b4f19
Anees <aneespengad4447@gmail.com>
2026-05-05 09:50:18 +0530

    feat(page): add navbar and hero components
    - integrate Navbar and Hero components into the main page
    - enhance user experience with a structured layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Hero from "@/components/hero/Hero";
+import Navbar from "@/components/navbar/Navbar";
 import React from "react";
 
 const page = () => {
-  return <div>PIXEL BYTE</div>;
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
+  );
 };
 
 export default page;
