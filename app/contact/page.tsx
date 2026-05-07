2bffcd9b2ee9599b843f48b1d63b7a297fa28756
Anees <aneespengad4447@gmail.com>
2026-05-07 15:12:17 +0530

    feat(contact): add contact page
    - implement new contact page component
    - include navbar, contact section, and footer components

---
+++ app/contact/page.tsx
@@ -0,0 +1,16 @@
+import ContactSection from "@/components/contact/ContactSection";
+import Footer from "@/components/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
+import React from "react";
+
+const ContactPage = () => {
+  return (
+    <main>
+      <Navbar />
+      <ContactSection />
+      <Footer />
+    </main>
+  );
+};
+
+export default ContactPage;
