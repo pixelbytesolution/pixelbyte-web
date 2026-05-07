c39f33ddf81811ad18b8570721a8f7cd203e46e7
Anees <aneespengad4447@gmail.com>
2026-05-07 15:11:35 +0530

    feat(careers): add careers page component
    - create a new careers page component
    - include navbar, careers section, and footer components
    - set up the page structure for career opportunities

---
+++ app/careers/page.tsx
@@ -0,0 +1,16 @@
+import CareersSection from "@/components/careers/CareersSection";
+import Footer from "@/components/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
+import React from "react";
+
+const CareersPage = () => {
+  return (
+    <main>
+      <Navbar />
+      <CareersSection />
+      <Footer />
+    </main>
+  );
+};
+
+export default CareersPage;
